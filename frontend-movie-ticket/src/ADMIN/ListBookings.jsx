18079a2b22310ff1f9e0b7dbe94ba3bea78b7fd1
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-12-28 00:47:47 +0530

    added ListBooking page

--- frontend-movie-ticket/src/ADMIN/ListBookings.jsx
+++ frontend-movie-ticket/src/ADMIN/ListBookings.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { dummyShowData } from "../assets/dummyShowCard";
 import { Loading } from "../components/Loading";
+import adminDummyDashboardData from "../assets/adminDummyDashboardData";
 
 function ListBookings() {
 
@@ -9,7 +9,7 @@ function ListBookings() {
   const [isloading, setIsLoading] = useState(true);
 
   const getAllBookings = async() => {
-    setBookings(dummyShowData);
+    setBookings(adminDummyDashboardData);
     setIsLoading(false);
   }
 
@@ -19,11 +19,11 @@ function ListBookings() {
 
   return !isloading ? (
     <div>
-      <Title tetx1="List" text2="Bookings" />
+      <Title textt1="List" text2="Bookings" />
       <div className="max-w-4xl mt-6 overflow-x-auto">
         <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
           <thead>
-            <tr className="bg-pink-600 text-left tetx-white">
+            <tr className="bg-pink-600 text-left text-white">
               <th className="p-2 font-medium pl-5">User Name</th>
               <th className="p-2 font-medium">Movie Name</th>
               <th className="p-2 font-medium">Show Time</th>
@@ -31,6 +31,21 @@ function ListBookings() {
               <th className="p-2 font-medium">Amount</th>
             </tr>
           </thead>
+
+          <tbody className="text-lg font-medium">
+            {
+              bookings.activeShows.map((item, index) => (
+                <tr key={index} className="border-b border-pink-600 bg-pink-600 even:bg-pink-600">
+                  <td className="p-2 min-w-45 pl-5">{item.username}</td>
+                  <td className="p-2">{item.movie.title}</td>
+                  <td className="p-2">{item.showDateTime}</td>
+                  <td className="p-2">{item.bookedSeats.map(seat => `${seat.seatNo} (${seat.type})`).join(', ')}</td>
+                  <td className="p-2">{currency} {item.showPrice}</td>
+
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
 
       </div>
@@ -39,3 +54,5 @@ function ListBookings() {
 }
 
 export default ListBookings
+
+// {Object.keys(item.bookedSeats).map(seat => bookedSeats[seat]).join(", ")}
--- frontend-movie-ticket/src/assets/adminDummyDashboardData.js
+++ frontend-movie-ticket/src/assets/adminDummyDashboardData.js
@@ -1,4 +1,3 @@
-
 const adminDummyDashboardData = {
   totalBookings: 1245,
   totalRevenue: 18500,
@@ -8,11 +7,17 @@ const adminDummyDashboardData = {
       _id: "show1",
       movie: {
         title: "The Lost Kingdom",
-        poster_path:"https://th.bing.com/th/id/OIP.ZaXgA6LqB_E5uL3D58DlJAHaLH?o=7&cb=ucfimg2&rm=3&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        poster_path: "https://th.bing.com/th/id/OIP.ZaXgA6LqB_E5uL3D58DlJAHaLH?o=7&cb=ucfimg2&rm=3&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
         vote_average: 8.8
       },
       showPrice: 250,
-      showDateTime: "2025-01-15"
+      showDateTime: "2025-01-15",
+      username: "Akshita",
+      bookedSeats: [
+        { seatNo: "A1", type: "VIP" },
+        { seatNo: "A2", type: "VIP" },
+        { seatNo: "B5", type: "Regular" }
+      ]
     },
     {
       _id: "show2",
@@ -22,47 +27,76 @@ const adminDummyDashboardData = {
         vote_average: 8.6
       },
       showPrice: 300,
-      showDateTime: "2025-01-20"
+      showDateTime: "2025-01-20",
+      username: "Rahul",
+      bookedSeats: [
+        { seatNo: "C3", type: "Regular" },
+        { seatNo: "C4", type: "Regular" }
+      ]
     },
     {
       _id: "show3",
       movie: {
         title: "Galactic Wars",
-        poster_path:"https://tse4.mm.bing.net/th/id/OIP.gkZhTVOXPHRoGml2Xhg0KgHaHa?cb=ucfimg2&pid=ImgDet&ucfimg=1&w=192&h=192&c=7&o=7&rm=3",
+        poster_path: "https://tse4.mm.bing.net/th/id/OIP.gkZhTVOXPHRoGml2Xhg0KgHaHa?cb=ucfimg2&pid=ImgDet&ucfimg=1&w=192&h=192&c=7&o=7&rm=3",
         vote_average: 9.0
       },
       showPrice: 280,
-      showDateTime: "2025-01-25"
+      showDateTime: "2025-01-25",
+      username: "Sneha",
+      bookedSeats: [
+        { seatNo: "D1", type: "VIP" },
+        { seatNo: "D2", type: "VIP" },
+        { seatNo: "E7", type: "Regular" },
+        { seatNo: "E8", type: "Regular" }
+      ]
     },
     {
       _id: "show4",
       movie: {
         title: "Whispers in the Dark",
-        poster_path:"https://www.themoviedb.org/t/p/original/c7n1fZuUMeEVspvzOrIUCz4XdCe.jpg",
+        poster_path: "https://www.themoviedb.org/t/p/original/c7n1fZuUMeEVspvzOrIUCz4XdCe.jpg",
         vote_average: 7.9
       },
       showPrice: 320,
-      showDateTime: "2025-01-28"
+      showDateTime: "2025-01-28",
+      username: "Vikram",
+      bookedSeats: [
+        { seatNo: "F10", type: "Regular" }
+      ]
     },
     {
       _id: "show5",
       movie: {
         title: "Neon Streets",
-        poster_path:"https://ik.imagekit.io/storybird/images/a7287dce-d004-4849-92a0-6e7541070bd0/0_714103755.png?tr=q-80",
+        poster_path: "https://ik.imagekit.io/storybird/images/a7287dce-d004-4849-92a0-6e7541070bd0/0_714103755.png?tr=q-80",
         vote_average: 8.3
       },
       showPrice: 270,
-      showDateTime: "2025-02-02"
+      showDateTime: "2025-02-02",
+      username: "Priya",
+      bookedSeats: [
+        { seatNo: "G1", type: "VIP" },
+        { seatNo: "G2", type: "VIP" },
+        { seatNo: "H3", type: "Regular" },
+        { seatNo: "H4", type: "Regular" },
+        { seatNo: "H5", type: "Regular" }
+      ]
     },
     {
       _id: "show6",
       movie: {
         title: "Beyond the Horizon",
-        poster_path:"https://th.bing.com/th/id/OIP.1_eKEDTpnFA3qbROs7xc7wHaLH?w=123&h=185&c=7&r=0&o=7&cb=ucfimg2&pid=1.7&rm=3&ucfimg=1",
+        poster_path: "https://th.bing.com/th/id/OIP.1_eKEDTpnFA3qbROs7xc7wHaLH?w=123&h=185&c=7&r=0&o=7&cb=ucfimg2&pid=1.7&rm=3&ucfimg=1",
         vote_average: 8.5
       },
       showPrice: 350,
-      showDateTime: "2025-02-05"
+      showDateTime: "2025-02-05",
+      username: "Arjun",
+      bookedSeats: [
+        { seatNo: "J2", type: "Regular" },
+        { seatNo: "J3", type: "Regular" }
+      ]
     }
   ]
 };
