d0b05d7cfd15941e2b6ae904379f9f8d6da1422c
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-12-31 16:48:21 +0530

    made changes in db-js and in env

--- backend-movie-ticket/config/db.js
+++ backend-movie-ticket/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
        // mongoose.connection.on('connected', () => console.log("Database connected"))
-        await mongoose.connect(`${process.env.MONGODB_URI}/movieTicket`)
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
         //await mongoose.connect(process.env.MONGODB_URI);
         console.log("Database connected...")
     } catch (error) {
