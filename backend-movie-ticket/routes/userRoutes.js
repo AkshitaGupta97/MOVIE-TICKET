9be422121c2f20012a9e976153b02117e9db7d1d
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-01-04 16:02:05 +0530

    added getFavouriteMovie

--- backend-movie-ticket/routes/userRoutes.js
+++ backend-movie-ticket/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserBookings, updateFavouriteMovie } from "../controllers/userController.js";
+import { getFavouriteMovies, getUserBookings, updateFavouriteMovie } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
