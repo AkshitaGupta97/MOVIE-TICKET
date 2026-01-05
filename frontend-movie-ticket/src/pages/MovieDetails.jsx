b976614f638d8932252bab75a7e41bee6c65dcd4
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-01-05 20:03:58 +0530

    added getShow

--- frontend-movie-ticket/src/pages/MovieDetails.jsx
+++ frontend-movie-ticket/src/pages/MovieDetails.jsx
@@ -1,30 +1,56 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
-import { dummyShowData } from "../assets/dummyShowCard";
+//import { dummyShowData } from "../assets/dummyShowCard";
 import BlurCircle from "../components/BlurCircle";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import timeFormat from "../lib/timeFormat";
-import dummyDateTimeData from "../assets/dummyDateTimeData";
+//import dummyDateTimeData from "../assets/dummyDateTimeData";
 import DateSelect from "../components/DateSelect";
 import MovieCard from "../components/MovieCard";
 import { Loading } from "../components/Loading";
 import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 function MovieDetails() {
   const navigate = useNavigate();
   const { id } = useParams(); // to get id from url
   const [show, setShow] = useState(null);
 
-  const {shows, axios, getToken, user, fetchfavouriteMovies, favouriteMovie, image_base_url} = useAppContext();
+  const {shows, axios, getToken, user, fetchFavouriteMovies, favouriteMovie, image_base_url} = useAppContext();
 
   // fetch show from id and store it in variable
   const getShow = async () => {
-    const showdata = dummyShowData.find(show => show._id === String(id));
+    /*const showdata = dummyShowData.find(show => show._id === String(id));
     if (showdata) {
       setShow({
         movie: showdata,
         dateTime: dummyDateTimeData,
       })
+    }*/
+    
+    try {
+      const {data} = await axios.get(`/api/show/${id}`);
+      if(data.success){
+        setShow(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }  
+
+  }
+
+  const handleFavourite = async() => {
+    try {
+      if(!user) return toast.error("Please login to add favourite movies");
+      const {data} = await axios.post('/api/user/update-favourite', {movieId: id}, {
+        headers: {Authorization: `Bearer ${await getToken()}`}
+      });
+      if(data.success){
+        await fetchFavouriteMovies();
+        toast.success(data.message);
+      }
+    } catch (error) {
+      toast.error("Failed to update favourite movies");
     }
   }
 
@@ -36,7 +62,7 @@ function MovieDetails() {
     <div className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
       <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto ">
 
-        <img src={show.movie.poster_path} alt="poster" className="max-md:mx-auto border-1 border-amber-300 rounded-xl h-100 max-w-70 object-cover" />
+        <img src={image_base_url + show.movie.poster_path} alt="poster" className="max-md:mx-auto border-1 border-amber-300 rounded-xl h-100 max-w-70 object-cover" />
 
         <div className="relative flex flex-col gap-3">
           <BlurCircle top="-100px" left="-100px" />
@@ -60,14 +86,13 @@ function MovieDetails() {
             </button>
             <a href="#dateSelect" className="px-10 py-3 text-sm bg-pink-600 hover:bg-pink-700 transition rounded-md 
             font-medium cursor-pointer active:scale-95">Buy Tickets</a>
-            <button className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
-              <Heart className={`w-5 h-5`} />
+            <button onClick={handleFavourite} className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
+              <Heart  className={`w-5 h-5 ${favouriteMovie.find(movie => movie._id === id) ?
+                'fill-pink-600' : ""
+              }`} />
             </button>
-
           </div>
-
         </div>
-
       </div>
 
       <p className="text-2xl text-amber-400 font-medium mt-20">Favourite Cast ....</p>
@@ -90,8 +115,8 @@ function MovieDetails() {
       <p className="text-2xl text-amber-500 font-medium mt-20 mb-8">Discover more...</p>
       <div className="flex flex-wrap max-sm:justify-center gap-8">
         {
-          dummyShowData.slice(0, 4).map((movie, index) => (
-            <MovieCard key={index} movie={movie} />
+          shows.slice(0, 4).map((mov, index) => (
+            <MovieCard key={index} movie={mov.movie} />
           ))
         }
       </div>
